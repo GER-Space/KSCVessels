4b231cb91468afebe3c978387e6e1d52c8d8b5fe
GER-Space <chris.github@bronk.de>
2019-12-07 23:22:13 +0100

    Initial commit

---
+++ src/KSCVessels.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KSCVessels
+{
+    public class KSCVessels : VesselModule
+    {
+
+        int rootIdx => vessel.protoVessel.rootIndex;
+        ProtoVessel protoVessel => vessel.protoVessel;
+
+        GameObject rootPart;
+
+        static SpaceCenterCamera2 spaceCenterCam;
+
+        List<GameObject> allObjects = new List<GameObject>();
+
+        new public void Start()
+        {
+            if (HighLogic.LoadedScene != GameScenes.SPACECENTER)
+            { 
+                return;
+            }
+
+
+            if (vessel.vesselType == VesselType.SpaceObject || vessel.vesselType == VesselType.Unknown)
+            {
+                return;
+            }
+            //Log.Normal("start called on: " + this.vessel.name);
+            //Log.Normal("RootPartindex: " + rootIdx);
+            if (spaceCenterCam == null)
+            {
+                spaceCenterCam = Resources.FindObjectsOfTypeAll<SpaceCenterCamera2>().FirstOrDefault();
+            }
+            if (Vector3.Distance(vessel.transform.position, spaceCenterCam.transform.position) < 8000)
+            {
+                LoadRootPart();
+                LoadOtherParts();
+            }
+
+        }
+
+
+        internal void LoadRootPart()
+        {
+            ProtoPartSnapshot rootProtoPart = protoVessel.protoPartSnapshots[rootIdx];
+            rootPart = GameObject.Instantiate(rootProtoPart.partPrefab.gameObject, vessel.transform.position, vessel.transform.rotation);
+            rootPart.gameObject.SetActive(true);
+            allObjects.Add(rootPart);
+        }
+
+        internal void LoadOtherParts()
+        {
+            for (int i = 0 ; i < protoVessel.protoPartSnapshots.Count; i++) 
+            {
+                // ignore the root part
+                if (i == rootIdx)
+                {
+                    continue;
+                }
+                ProtoPartSnapshot protoPart = protoVessel.protoPartSnapshots[i];
+                GameObject part = GameObject.Instantiate(protoPart.partPrefab.gameObject, vessel.transform.position, vessel.transform.rotation);
+                part.transform.parent = rootPart.transform;
+                part.transform.localPosition = protoPart.position;
+                part.transform.localRotation = protoPart.rotation;
+                part.gameObject.SetActive(true);
+                allObjects.Add(part);
+            }
+        }
+
+
+        public void OnDestroy()
+        {
+            foreach (GameObject obj in allObjects)
+            {
+                obj.DestroyGameObject();
+            }
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KSCVessels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KSCVessels")]
+[assembly: AssemblyCopyright("Copyright Christian Bronk ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b02f3ba5-9466-40fe-9185-71d4f65ff9b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.8.1.0")]
+[assembly: AssemblyInformationalVersion("1.8.1.0")]
+[assembly: KSPAssembly("KSCVessels", 1, 0)]
